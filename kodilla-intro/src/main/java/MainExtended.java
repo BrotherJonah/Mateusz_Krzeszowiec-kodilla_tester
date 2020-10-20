a9a3e86df28daa54f07c05be7b396bcee9230b08
Mateusz Krzeszowiec <krzeszowiecm2@gmail.com>
2020-10-20 20:11:52 +0200

    1.6 Podsumowanie

--- kodilla-intro/src/main/java/MainExtended.java
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class MainExtended {
+public class MainExtended {
+    public static void main(String[] args){
+        String name="Michał";
+        String surname="Kowaleczko";
+        int age=21;
+        double height=2.11;
+        boolean smart=true;
+        char girlOpinion='A';
+
+        System.out.println(name);
+        System.out.println(surname);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(smart);
+        System.out.println(girlOpinion);
+    }
 }
--- kodilla-intro/src/main/java/SimpleArray.java
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class SimpleArray {
+public class SimpleArray {
+    public static void main(String[] args) {
+        String[] names = new String[] {"Kasia", "Basia", "Zosia", "Marysia", "Michał"};
+        String[] movies = new String[4];
+
+        String name = names[3];
+        System.out.println(name);
+
+        int numberOfElements = names.length;
+        int numberOfElements1 = movies.length;
+
+        System.out.println("Ilość elementów w tablicy1: ");
+        System.out.println(numberOfElements);
+        System.out.println("Ilość elementów w tablicy2: ");
+        System.out.println(numberOfElements1);
+
+        movies[0] = "Harry Potter";
+        movies[1] = "Lord of the Rings";
+        movies[2] = "Forrest Gump";
+        movies[3] = "Day Potatoe";
+
+        System.out.println("\n Elementy będące w tablicy names: ");
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+            }
+
+        System.out.println("\n A tu od końca: ");
+        for (int i = names.length - 1; i >= 0; i--) {
+            System.out.println(names[i]);
+        }
+    }
 }
