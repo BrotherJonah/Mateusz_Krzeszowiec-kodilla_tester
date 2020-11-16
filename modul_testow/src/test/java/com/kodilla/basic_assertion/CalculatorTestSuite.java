a912f41aab6717f889c8584c5c32cce3436fc37b
Mateusz Krzeszowiec <krzeszowiecm2@gmail.com>
2020-11-16 10:56:04 +0100

    Zadanie: testy jednostkowe dla kalkulatora

--- modul_testow/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ modul_testow/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -1,2 +1,42 @@
-package com.kodilla.basic_assertion;public class CalculatorTestSuite {
+package com.kodilla.basic_assertion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTestSuite {
+
+    @Test
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a, b);
+        assertEquals(13, sumResult);
+    }
+
+    @Test
+    public void testSub() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int subResult = calculator.subtract(a, b);
+        assertEquals(-3, subResult);
+    }
+
+    @Test
+    public void testToSquare() {
+        Calculator calculator = new Calculator();
+        int a = -3;
+        int b = 2;
+        int c = 0;
+        int toSquareResultA = calculator.toSquare(a);
+        assertEquals(9, toSquareResultA);
+
+        int toSquareResultB = calculator.toSquare(b);
+        assertEquals(4, toSquareResultB);
+
+        int toSquareResultC = calculator.toSquare(c);
+        assertEquals(0, toSquareResultC);
+    }
 }
