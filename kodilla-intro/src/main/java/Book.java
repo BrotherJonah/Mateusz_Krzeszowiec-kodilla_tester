00cfcc08c4fb76bd12b84311110ae510181f06e8
Mateusz Krzeszowiec <krzeszowiecm2@gmail.com>
2020-11-19 22:50:08 +0100

    Zadanie: zmienne i metody statyczne

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Book {
+public class Book {
+    private String author;
+    private String title;
+
+    static String of(String author, String title) {
+        this.author = author;
+        this.title = title;
+
+        return author + title;
+    }
+
+    public static void main(String[] args) {
+        Book book = Book.of("Isaac Asimov", "The Galaxy");
+
+    }
 }
